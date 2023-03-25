c3c830bb09ee8363beea46a50c9cac73a295389d
Ezequiel Mamone <103384030+jazz3run@users.noreply.github.com>
2023-03-25 02:44:04 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,39 +43,88 @@ window.addEventListener('scroll', scrollHeader)
 const sections = document.querySelectorAll('section[id]')
     
 const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
+    const scrollY = window.pageYOffset
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
 }
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
+    const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form');
+const calculateCm = document.getElementById('calculate-cm');
+const calculateKg = document.getElementById('calculate-kg');
+const calculateMessage = document.getElementById('calculate-message');
 
+const calculateBmi = (e) =>{
+    e.preventDefault()
 
+    // Check if the fields have a value
+    if(calculateCm.value === '' || calculateKg.value === ''){
+        // Add and remove color
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        // Show menssage
+        calculateMessage.textContent = 'Llenar los datos requeridos🤷‍'
+
+        // Remove message three seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else{
+        // BMI Formula
+        const cm = calculateCm.value / 100;
+        const kg = calculateKg.value;
+        const bmi = Math.round(kg / (cm * cm));
+
+        // Show your health status
+        if(bmi < 18.5){
+            //Add color and display message
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Tu IMC es ${bmi} y estas bajo de peso 😓`
+        } else if(bmi < 25){
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Tu IMC es ${bmi} y estas saludable 😉`
+        } else{
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Tu IMC es ${bmi} y estas con peso elevado 😔`
+        }
+
+        // To clear the input field
+        calculateCm.value = '';
+        calculateKg.value = '';
+
+        // Remove the message after 4 seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 4000)
+    }
+}
+
+calculateForm.addEventListener('submit', calculateBmi);
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('footer-form');
 const contactMessage = document.getElementById('contact-message');
